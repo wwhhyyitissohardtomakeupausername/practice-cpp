26ad3b6d3728b9355e2d5c29dc7390ffeffce478
qingyang <u202613798@hust.edu.cn>
2026-09-19 23:28:13 +0800

    9.19_23:28

---
+++ class_bl.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+class box {
+    public:
+        double Getlength() {return length;}
+        void Setlength(double length) {this->length=length;}
+        
+    private:
+        double length,breadth,height;
+};
+int main() {
+    
+}
